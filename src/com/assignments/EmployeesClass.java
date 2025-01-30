ce7154bc1f27b93de5f83233427013b7d2c25a69
Umamaheswari <umakumaran2028@gmail.com>
2025-01-30 23:53:54 +0530

    EmployeesClass

---
+++ src/com/assignments/EmployeesClass.java
@@ -0,0 +1,23 @@
+package com.assignments;
+
+public class EmployeesClass {
+
+	//Create an Employees class that stores the following employee names and IDs in separate variables:
+		// Employee Names: "Uma", "Sun", "Ram"
+		//Employee IDs: 1234, 1235, 1236
+	
+	    String empName1 = "Uma";
+	    String empName2 = "Sun";
+	    String empName3 = "Ram";
+	    
+	    int empID1 = 1234;
+	    int empID2 = 1235;
+	    int empID3 = 1236;
+	
+	//create another class called EmpGroups that contains two arrays to store the employee names and IDs.
+	
+	public static void main(String[] args) {
+	
+	}
+
+}

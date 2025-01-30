5f078b5e353812730bf9bf8b05ca0daa0db7c0bc
Umamaheswari <umakumaran2028@gmail.com>
2025-01-30 07:47:10 +0530

    Assignment 2 Arrays

--- src/com/assignments/Assignment2Arrays.java
+++ src/com/assignments/Assignment2Arrays.java
@@ -13,53 +13,38 @@ public class Assignment2Arrays {
 		
 		// Step 1: Create 3D arrays for subject names and marks
 		
-		 String[][][] subjectnames = {
+		 String[][][] subjectMarks = {
 
-	{  //Semester 1 Subject Names	
-		{"Mathematics I"},{"Physics"},{"Chemistry"},{"Computer Programming"},{"Engineering Drawing"},{"Basic Electrical Eng."}
+	{  //Semester 1 Subject Names and Marks	
+		{"Mathematics I", "78"}, {"Physics", "85"},{"Chemistry", "91"},
+		{"Computer Programming", "74"},{"Engineering Drawing","88"},{"Basic Electrical Eng","79"}
 	},
-	{  //Semester 2 Subject Names
-		{"Mathematics II"},{"Mechanics"},{"Environmental sci"},{"Basic Electronics"},{"Engineering Physics"},{"Engineering Graphics"}
+	{  //Semester 2 Subject Names and Marks 
+		{"Mathematics II", "82"},{"Mechanics","77"},{"Environmental sci","93"},
+		{"Basic Electronics","69"},{"Engineering Physics","84"},{"Engineering Graphics","90"}
 	},
-	{  //Semester 3 Subject Names
-		{"Data Structures"},{"Discrete Mathematics"},{"Digital Electronics"}, {"Operating Systems"}, {"Signals and Systems"},{"Object-Oriented Prog"}
+	{  //Semester 3 Subject Names and Marks
+		{"Data Structures","88"},{"Discrete Mathematics","81"},{"Digital Electronics","76"}, 
+		{"Operating Systems","92"}, {"Signals and Systems","85"},{"Object-Oriented Prog","78"}
 	},
-	{  //Semester 4 Subject Names
-		{"Algorithms"}, {"Computer Networks"},{"Database Systems"}, {"Microprocessors"},{"Communication Eng"}, {"Software Engineering"}
+	{  //Semester 4 Subject Names and Marks
+		{"Algorithms","91"}, {"Computer Networks","73"},{"Database Systems","89"}, 
+		{"Microprocessors","80"},{"Communication Eng","76"}, {"Software Engineering","87"}
 	},
-	{  //Semester 5 Subject Names
-		{"Probability & Stats"},{"Machine Learning"},{"Compiler Design"},{"Theory of Computation"}, {"Embedded Systems"}, {"Computer Graphics"}
+	{  //Semester 5 Subject Names and Marks
+		{"Probability & Stats","86"},{"Machine Learning","88"},{"Compiler Design","84"},
+		{"Theory of Computation","95"}, {"Embedded Systems","73"}, {"Computer Graphics","90"}
 	}
 };
 	
-         int[][][] Marks = {
-        		 
-     { //Semester 1 marks 		 
-    	 {78, 85, 91, 74,88, 79} 
-     },
-     { //Semester 2 marks
-    	 {82, 77, 93, 69,84, 90}
-     },
-     { //Semester 3 marks
-    	 {88, 81, 76, 92, 85, 78}
-     },
-     { //Semester 4 marks
-    	 {91, 73, 89, 80, 76, 87}
-     },
-     { //Semester 5 marks
-    	 {86, 88, 84, 95, 73, 90}
-     }
-     
- };
+     //From the array:
+         //Print Semester 3 - Subject 4 and Subject 5 Names and //Print Semester 5 - Subject 3 and Subject 6 marks  
+         System.out.println("Semester 3- Subject 4:" + subjectMarks[2][3][0]);
+         System.out.println("Semester 3- Subject 5:" + subjectMarks[2][4][0]);
+         System.out.println("Semester 5- Subject 3:" + subjectMarks[4][2][1]);
+         System.out.println("Semester 5- Subject 6:" + subjectMarks[4][5][1]);
+           
          
-   //From the array:
-         //Print Semester 3 - Subject 4 and Subject 5 Names.
-         System.out.printf("Semester 3- Subject 4: %s%n", subjectnames[2][3][0]);
-         System.out.printf("Semester 3- Subject 5: %s%n", subjectnames[2][4][0]);
-  
-         //Print Semester 5 - Subject 3 and Subject 6 marks      
-         System.out.printf("Semester 5- Subject 3: %d%n",Marks[4][0][2]);
-         System.out.printf("Semester 5-Subject 6: %d%n", Marks[4][0][5]);
        
         
 }
